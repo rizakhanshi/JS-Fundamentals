f251f14b65ad84d4a12717c9eeb13ce18c32f6d2
Abdirizack Hanshi <148985083+rizakhanshi@users.noreply.github.com>
2025-07-13 23:47:38 +0300

    Create  5-to_integer.js

---
+++ 5-to_integer.js
@@ -0,0 +1,10 @@
+// 5-to_integer.js
+
+const arg = process.argv[2];
+const num = parseInt(arg);
+
+if (isNaN(num)) {
+  console.log("Not a number");
+} else {
+  console.log(`My number: ${num}`);
+}
